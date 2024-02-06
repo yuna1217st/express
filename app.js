337dfc5587c6f0fa7748787587706fdc04326fb2
yuna1217st <2101320370xs@cyber-u.ac.jp>
2024-02-07 01:18:02 +0900

    lesson06

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
-var notesRouter = require('./routes/notes');
+var notes_L06Router = require('./routes/notes_L06');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
-app.use('/notes', notesRouter);
+app.use('/notes_L06', notes_L06Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
